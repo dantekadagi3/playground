a181bf4a3a7ed9019a3bc5d03ba81ffa76a8f11a
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-04-08 22:34:11 -0700

    tuesday

---
+++ switch.js
@@ -0,0 +1,31 @@
+var day ="Sunday"
+
+switch(day){
+    case "Monday":
+        console.log("Code in javascript")
+        break;
+
+    case "Tuesday"  :
+        console.log("Do a css exercise") 
+       break; 
+    case "Wednesday"  :
+            console.log("Do a css exercise") 
+        break; 
+    case "Thursday"  :
+                console.log("Do a css exercise") 
+        break; 
+
+        case "Friday"  :
+            console.log("Do a css exercise") 
+            break; 
+
+            case "Saturday"  :
+                console.log("Do a css exercise") 
+                break; 
+                case "Sunday"  :
+                    console.log("Do a css exercise") 
+                    break; 
+           default:
+            console.log("There is no such day")
+    
+}
