be50f3a3bb6d3686289f23ac60a6ac348880471e
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-04-09 23:39:47 -0700

    day 3

---
+++ counterapp.html/nestedloops.js
@@ -0,0 +1,50 @@
+for (var i=1;i<=2;i++){
+    for(var j=1;j<=5;j++){
+        console.log('')
+        console.log("week" +i+"day" +j)
+    }
+}
+
+for(var year=2023 ;year<2025; year++){
+    console.log(year);
+for(var month=6;month<9;month++){
+    console.log("--------",month)
+}
+}
+
+for(var i=1;i<=10;i++){
+    if(i==1){
+        console.log("Gold medal")
+    }else if(i==2){
+        console.log("Silver Medal")
+    }else if(i==3){
+        console.log("bronze Medal")
+    }else{
+        console.log(i)
+    }
+}
+for (var i = 1; i <= 10; i++) {
+    switch (i) {
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver Medal");
+            break;
+        case 3:
+            console.log("Bronze Medal");
+            break;
+        default:
+            console.log(i);
+            
+    }
+}
+for (i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log("Hello");
+    }
+}
+for(var i = 0; i <= 5; i++) {
+    console.log("Hello");
+  }
+
