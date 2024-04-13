bb7b29841931cca14a9ec99b0bbd573a961b08ab
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-04-13 07:57:16 -0700

    objects

---
+++ counterapp.html/objects.js
@@ -0,0 +1,24 @@
+//declaring objects in dot notation
+var user={}
+user.home="kajiado",
+user.health=40,
+user.wealth=1000000
+console.log(user.wealth)
+
+//key-value pairs notation
+var assistantManager={
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
+console.log(assistantManager.health)
+
+//object literals and the brackets notation
+var house2 = {};
+house2["rooms"] = 4;
+house2['color']= "pink";
+house2["priceUSD"] = 12345;
+console.log(house2); 
